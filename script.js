913f5c7cb28dfaa07f160632c0571763fcc55c94
Matthew Clockel <35537679+Matt87c@users.noreply.github.com>
2020-05-18 22:40:34 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
-// Write your JavaScript code here!
-
 window.addEventListener("load", function () {
   fetch("https://handlers.education.launchcode.org/static/planets.json").then(
     function (response) {
       response.json().then(function (json) {
         const div = document.getElementById("missionTarget");
+        
         // Add HTML that includes the JSON data
         let randomIndex = Math.floor(Math.random() * json.length);
         div.innerHTML = `
-
 <h2>Mission Destination</h2>
 <ol>
    <li>Name: ${json[randomIndex].name}</li>
@@ -41,46 +39,58 @@ window.addEventListener("load", function () {
       alert("Invalid input. All fields are required.");
       event.preventDefault();
     }
+    
     if (!isNaN(pilotInput.value)) {
       alert("Invalid input. Pilot name must be a string.");
       event.preventDefault();
     }
+    
     if (!isNaN(coPilotInput.value)) {
       alert("Invalid input. Co-pilot name must be a string.");
       event.preventDefault();
     }
+    
     if (isNaN(fuelInput.value)) {
       alert("Invalid input. Fuel level must be a number.");
       event.preventDefault();
     }
+    
     if (isNaN(cargoInput.value)) {
       alert("Invalid input. Cargo mass must be a number.");
       event.preventDefault();
     }
+    
     let pilotStatus = document.getElementById("pilotStatus");
     pilotStatus.innerText = `${pilotInput.value}`;
+    
     let coPilot = document.getElementById("copilotStatus");
     coPilot.innerText = `${coPilotInput.value}`;
 
     if (parseInt(fuelInput.value) < 10000) {
       let faultyItemsElement = document.getElementById("faultyItems");
       faultyItemsElement.style.visibility = "visible";
+      
       let fuelStatusElement = document.getElementById("fuelStatus");
       fuelStatusElement.innerText = "There is not enough fuel for the journey.";
+      
       let launchStatus = document.getElementById("launchStatus");
       launchStatus.innerText = "Shuttle not ready for launch.";
       launchStatus.style.color = "red";
       event.preventDefault();
     }
+    
     if (parseInt(cargoInput.value) > 10000) {
       let faultyItemsElement = document.getElementById("faultyItems");
       faultyItemsElement.style.visibility = "visible";
+      
       let cargoStatus = document.getElementById("cargoStatus");
       cargoStatus.innerText = "There is too much mass";
+      
       let launchStatus = document.getElementById("launchStatus");
       launchStatus.innerText = "Shuttle not ready for launch.";
       launchStatus.style.color = "red";
       event.preventDefault();
+    
     } else {
       let launchStatus = document.getElementById("launchStatus");
       launchStatus.innerText = "Shuttle is ready for launch.";
